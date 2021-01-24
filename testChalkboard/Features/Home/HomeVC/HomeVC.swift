798d11578a8a4be9ad412121f8984119a41a9b6d
Gerard <gerar.riera@gmail.com>
2021-01-24 16:08:42 +0100

    cbt-5: - Integrating home

--- testChalkboard/Features/Home/HomeVC/HomeVC.swift
+++ testChalkboard/Features/Home/HomeVC/HomeVC.swift
@@ -8,8 +8,37 @@
 import UIKit
 
 class HomeVC: BaseVC {
+    
+    @IBOutlet weak var mainTV: UITableView! {
+        didSet {
+            
+            mainTV.showsVerticalScrollIndicator = false
+            mainTV.rowHeight = 386.0
+            mainTV.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            mainTV.delegate = self
+            mainTV.dataSource = self
+            mainTV.separatorStyle = .none
+        }
+    }
+    
+    var birthdays = [Birthday]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func cellForBirthday(at indexPath: IndexPath) {
+        
+    }
+}
+
+extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return birthdays.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cellForBirthday(at: indexPath)
     }
 }
 
