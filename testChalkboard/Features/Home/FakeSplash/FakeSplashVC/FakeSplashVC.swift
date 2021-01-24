027f56e492fe53fca7ef2f70bc7dd8689dc59e61
Gerard <gerar.riera@gmail.com>
2021-01-24 15:04:47 +0100

    cbt-1: - WIP Set-up base project & create commons

---
+++ testChalkboard/Features/Home/FakeSplash/FakeSplashVC/FakeSplashVC.swift
@@ -0,0 +1,8 @@
+//
+//  FakeSplashVC.swift
+//  testChalkboard
+//
+//  Created by Gerard Riera  on 24/01/2021.
+//
+
+import Foundation
---
+++ testChalkboard/Features/Home/FakeSplash/FakeSplashVM/FakeSplashVM.swift
@@ -0,0 +1,8 @@
+//
+//  FakeSplashVM.swift
+//  testChalkboard
+//
+//  Created by Gerard Riera  on 24/01/2021.
+//
+
+import Foundation
